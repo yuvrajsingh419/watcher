d752565bab17436cf256ac9a9e048c0f189ef1cf
Yuvraj Singh <yuvrajsingh1619@gmail.com>
2023-04-09 11:46:49 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var s = document.getElementsByClassName('sk');
+var d = document.getElementsByClassName('ks');
+var start = document.getElementsByClassName('p');
+var  dest = document.getElementsByClassName('pd');
+
+start.addEventListner
+
+if (s){
+        
+}
+
